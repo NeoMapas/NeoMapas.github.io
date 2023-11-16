780e64c026179fe34edc9fc924ac28c9f380b1c9
JR Ferrer-Paris <jr.ferrer.paris@gmail.com>
2023-11-17 09:55:16 +1100

    image magick for some fotos

--- how2/prepare-fotos.sh
+++ how2/prepare-fotos.sh
@@ -8,5 +8,7 @@
 # personas/JR-en-Araya.jpg
 
 ## Disponibles en el respaldo en gaia.ad.unsw.edu.au
-convert NeoMapas_imagenes/Ardaris/00_GavetaOrdenada.jpg -resize 400x400 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Ardaris-gaveta-ordenada.jpg
-convert NeoMapas_imagenes/CatastictaRevancha.png -resize 400x400 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Catasticta-revancha.jpg
+convert NeoMapas_imagenes/Ardaris/00_GavetaOrdenada.jpg -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Ardaris-gaveta-ordenada.jpg
+convert NeoMapas_imagenes/CatastictaRevancha.png -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Catasticta-revancha.jpg
+convert NeoMapas_imagenes/dgazella/Fig1_Digitonthophagus_gazella.png -resize 600x600 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Digitonthophagus-gazella.jpg
+convert NeoMapas_imagenes/ColecciónNeoMapas/ColeccionNeoMapas2.png -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Oxysternon-festivum.jpg
