ad9fa96f125cac1f53cfa794b5cf30e081391e93
JR Ferrer-Paris <jr.ferrer.paris@gmail.com>
2023-11-16 16:46:23 +1100

    añadir más fotos y migrar más páginas

---
+++ how2/prepare-fotos.sh
@@ -0,0 +1,12 @@
+# Descargadas de internet
+# personas/gustavo.jpg
+
+# enlazadas desde alguna dirección web
+#Personas/Ada
+
+# Encontradas en nuestros respaldos, aunque no recuerdo de donde
+# personas/JR-en-Araya.jpg
+
+## Disponibles en el respaldo en gaia.ad.unsw.edu.au
+convert NeoMapas_imagenes/Ardaris/00_GavetaOrdenada.jpg -resize 400x400 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Ardaris-gaveta-ordenada.jpg
+convert NeoMapas_imagenes/CatastictaRevancha.png -resize 400x400 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Catasticta-revancha.jpg
