130bf73ea211710322ea39cc58eb37cb8e98ecfb
Jose Rafael Ferrer-Paris <jr.ferrer.paris@gmail.com>
2023-11-19 20:04:16 +1100

    arreglando archivo

--- how2/prepare-fotos.sh
+++ how2/prepare-fotos.sh
@@ -8,9 +8,14 @@
 # personas/JR-en-Araya.jpg
 
 ## Disponibles en el respaldo en gaia.ad.unsw.edu.au
-<<<<<<< HEAD
+
+## Taxa
 convert NeoMapas_imagenes/Ardaris/00_GavetaOrdenada.jpg -resize 400x400 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Ardaris-gaveta-ordenada.jpg
 convert NeoMapas_imagenes/CatastictaRevancha.png -resize 400x400 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Catasticta-revancha.jpg
+convert NeoMapas_imagenes/Ardaris/00_GavetaOrdenada.jpg -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Ardaris-gaveta-ordenada.jpg
+convert NeoMapas_imagenes/CatastictaRevancha.png -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Catasticta-revancha.jpg
+convert NeoMapas_imagenes/dgazella/Fig1_Digitonthophagus_gazella.png -resize 600x600 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Digitonthophagus-gazella.jpg
+convert NeoMapas_imagenes/ColecciónNeoMapas/ColeccionNeoMapas2.png -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Oxysternon-festivum.jpg
 
 ## Disponibles en Teradactylo
 FOTOTECA=/Volumes/Teradactylo/Fototeca
@@ -46,9 +51,4 @@ $FOTOTECA/$FOTODIR/
 ## upload to inat:
 exiftool $FOTOTECA/$FOTODIR/Foto20041002141354.JPG
 
-=======
-convert NeoMapas_imagenes/Ardaris/00_GavetaOrdenada.jpg -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Ardaris-gaveta-ordenada.jpg
-convert NeoMapas_imagenes/CatastictaRevancha.png -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Catasticta-revancha.jpg
-convert NeoMapas_imagenes/dgazella/Fig1_Digitonthophagus_gazella.png -resize 600x600 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Digitonthophagus-gazella.jpg
-convert NeoMapas_imagenes/ColecciónNeoMapas/ColeccionNeoMapas2.png -resize 500x500 ~/proyectos/NeoMapas/NeoMapas.github.io/taxa/Oxysternon-festivum.jpg
->>>>>>> 780e64c026179fe34edc9fc924ac28c9f380b1c9
+
